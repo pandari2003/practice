8f2e09c1ed25c80a60ca9a824e46e57c8d544ca6
pandari <Pandari@telezertechnologies.com>
2026-07-18 23:46:24 +0530

    fixing

--- testing/testing.ino
+++ testing/testing.ino
@@ -36,14 +36,14 @@ void Sit()
         // Left pair
         servoH2.write(i);
         servoH3.write(i);
-        servoKN2.write(i);
-        servoKN3.write(i);
+        servoKN2.write(30+i);
+        servoKN3.write(30+i);
 
         // Right pair
         servoH1.write(180 - i);
         servoH4.write(180 - i);
-        servoKN1.write(180 - i);
-        servoKN4.write(180 - i);
+        servoKN1.write(150 - i);
+        servoKN4.write(150 - i);
 
         delay(50);      // Same speed as Stand
     }
