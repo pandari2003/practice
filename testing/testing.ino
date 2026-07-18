256c51cc0ad8f989b6c535f7e384eb91d76bb2f4
pandari <Pandari@telezertechnologies.com>
2026-07-19 03:18:11 +0530

    fixing

--- testing/testing.ino
+++ testing/testing.ino
@@ -32,10 +32,10 @@ const int servoKN4Pin = 13; // front left (servo 10)
 
 /************************left move***************/
 void Leftmove(){
-    for (int step = 0; step < 5; step++)
+    for (int step = 0; step < 2; step++)
   {
     //---------------- STEP 1 ----------------//
-    for (int i = 110; i >= 70; i--)
+    for (int i = 110; i >= 60; i--)
     {
       servoB1.write(i);
       servoB3.write(i);
@@ -46,7 +46,7 @@ void Leftmove(){
       delay(50);
     }
     delay(40);
-        for (int i = 70; i <= 110; i++)
+        for (int i = 60; i <= 110; i++)
     {
       servoB1.write(i);
       servoB3.write(i);
