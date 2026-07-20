139213a7313a1e384d58fdbd3d9a7325332d2c9a
pandari <Pandari@telezertechnologies.com>
2026-07-21 02:57:12 +0530

    left move

--- testing/testing.ino
+++ testing/testing.ino
@@ -100,18 +100,18 @@ const int servoKN4Pin = 13; // front left (servo 10)
 }*/
 
 void Sit() {
-  for (int i = 90; i >= 0; i--) {
+  for (int i = 45; i >= 0; i--) {
     // Left pair
     servoH2.write(i);
     servoH3.write(i);
-    servoKN2.write(i);
-    servoKN3.write(i);
+    servoKN2.write(i+45);
+    servoKN3.write(i+45);
 
     // Right pair
     servoH1.write(180 - i);
     servoH4.write(180 - i);
-    servoKN1.write(180 - i);
-    servoKN4.write(180 - i);
+    servoKN1.write(135 - i);
+    servoKN4.write(135 - i);
 
     delay(50);  // Same speed as Stand
   }
