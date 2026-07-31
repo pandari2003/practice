3f219f3c2f3818e5ff2cead952e67db9fd35a458
pandari <Pandari@telezertechnologies.com>
2026-08-01 03:42:40 +0530

    forword fixing.

--- testing/testing.ino
+++ testing/testing.ino
@@ -41,8 +41,8 @@ void walkForward() {
       
       servoHa3.write(i);
       servoHa4.write(i);
-      servoHa1.write(180 - i);
-      servoHa2.write(180 - i);
+      servoHa1.write(i-180);
+      servoHa2.write(i-180);
 
       delay(150);
     }
@@ -50,8 +50,8 @@ void walkForward() {
       
       servoHa3.write(i);
       servoHa4.write(i);
-      servoHa1.write(180 - i);
-      servoHa2.write(180 - i);
+      servoHa1.write(i-180);
+      servoHa2.write(i-180);
 
       delay(150);
     }
